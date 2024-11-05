f3c5c03a5763ba1ca6fa43b8ee2c0a077c41d546
章文俊 <1346502061@qq.com>
2024-11-05 11:17:20 +0800

    Update:Demo

--- Test/index.js
+++ Test/index.js
@@ -1,21 +1,7 @@
-import init, { ConvertPngImageFileToBase64 } from '../Build/index.js';
+import init, { Demo } from '../Build/index.js';
 
 await init(new Object());
 
-const input = document.querySelector('.Input');
-
-input.addEventListener('change', (e) => {
-    const file = e.target.files[0];
-    const reader = new FileReader();
-    reader.readAsArrayBuffer(file);
-    reader.onload = () => {
-        const pngBuffer = new Uint8Array(reader.result);
-        ConvertPngImageFileToBase64(pngBuffer)
-            .then((res) => {
-                console.log(res);
-            })
-            .catch((err) => {
-                console.error('err:', err);
-            });
-    };
+Demo().then(() => {
+    console.log('Wasm');
 });
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,4 @@
 #![allow(non_snake_case)]
-use base64::{engine::general_purpose, Engine};
-use image::DynamicImage;
-use std::io::{Cursor, Read, Seek, SeekFrom};
 use wasm_bindgen::prelude::*;
 
 #[cfg(feature = "wee_alloc")]
@@ -15,33 +12,6 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub async fn ConvertPngImageFileToBase64(buf: &[u8]) -> Result<String, String> {
-    match GetImageFromBuffer(&buf) {
-        Ok(img) => match GetBase64FromImage(&img) {
-            Ok(b) => Ok(b),
-            Err(_) => Err(format!("转换base64失败")),
-        },
-        Err(_) => Err(format!("请传入png图片")),
-    }
-}
-
-pub fn GetImageFromBuffer(buf: &[u8]) -> Result<DynamicImage, JsValue> {
-    match image::load_from_memory_with_format(buf, image::ImageFormat::Png) {
-        Ok(img) => Ok(img),
-        Err(_) => Err(JsValue::null()),
-    }
-}
-
-pub fn GetBase64FromImage(img: &DynamicImage) -> Result<String, JsValue> {
-    let mut c = Cursor::new(Vec::new());
-    match img.write_to(&mut c, image::ImageFormat::Png) {
-        Ok(_) => {
-            c.seek(SeekFrom::Start(0)).unwrap();
-            let mut out = Vec::new();
-            c.read_to_end(&mut out).unwrap();
-            let b = general_purpose::STANDARD.encode(&mut out);
-            Ok(format!("{}{}", "data:image/png;base64,", b))
-        }
-        Err(_) => Err(JsValue::null()),
-    }
+pub async fn Demo() -> Result<(), String> {
+    Ok(())
 }
