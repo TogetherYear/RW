d3299c22339c1d05ca96ba0b40853ca6db5ac9de
章文俊 <1346502061@qq.com>
2024-11-05 09:41:10 +0800

    Update:Format

--- Test/index.js
+++ Test/index.js
@@ -1,5 +1,16 @@
-import init, { Run } from '../Build/Color.js';
+import init, { Run, ConvertImage } from '../Build/index.js';
 
 await init(new Object());
 
-console.log(Run(''));
+const input = document.querySelector('.Input');
+
+input.addEventListener('change', (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsArrayBuffer(file);
+    reader.onload = () => {
+        const uint8Array = new Uint8Array(reader.result);
+        let s = ConvertImage(uint8Array);
+        console.log(s);
+    };
+});
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,48 @@
 #![allow(non_snake_case)]
+use std::io::{Cursor, Read, Seek, SeekFrom};
+
+use base64::{engine::general_purpose, Engine};
+use image::DynamicImage;
 use wasm_bindgen::prelude::*;
 
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
 #[wasm_bindgen]
-pub fn Run(_e: &JsValue) -> String {
-    String::from("#ffffff")
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn ConvertImage(buf: &[u8]) -> Result<String, JsValue> {
+    match GetImageFromBuffer(&buf) {
+        Ok(img) => match GetBase64FromImage(&img) {
+            Ok(b) => Ok(b),
+            Err(_) => Err(JsValue::null()),
+        },
+        Err(_) => Err(JsValue::null()),
+    }
+}
+
+pub fn GetImageFromBuffer(buf: &[u8]) -> Result<DynamicImage, JsValue> {
+    match image::load_from_memory_with_format(buf, image::ImageFormat::Png) {
+        Ok(img) => Ok(img),
+        Err(_) => Err(JsValue::null()),
+    }
+}
+
+pub fn GetBase64FromImage(img: &DynamicImage) -> Result<String, JsValue> {
+    let mut c = Cursor::new(Vec::new());
+    match img.write_to(&mut c, image::ImageFormat::Png) {
+        Ok(_) => {
+            c.seek(SeekFrom::Start(0)).unwrap();
+            let mut out = Vec::new();
+            c.read_to_end(&mut out).unwrap();
+            let b = general_purpose::STANDARD.encode(&mut out);
+            Ok(format!("{}{}", "data:image/png;base64,", b))
+        }
+        Err(_) => Err(JsValue::null()),
+    }
 }
